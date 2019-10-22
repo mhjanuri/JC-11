5f9169d1d0be7b43e80bec68c59530e055caf1f2
Hendri Januri <49939976+hendrijanuri@users.noreply.github.com>
2019-10-22 21:46:14 +0700

    clean code

---
+++ day02/solve05.js
@@ -0,0 +1,5 @@
+var sumUmur = 49
+var andi = (sumUmur * 40 / 100)
+var budi = (sumUmur * 60 / 100)
+console.log('Umur Andi setelah 2 tahun adalah ' + Math.round(andi + 2))
+console.log('Umur Budi setelah 2 tahun adalah ' + Math.round(budi + 2))
