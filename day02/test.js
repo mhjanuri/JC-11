c5f2286c3cd7fd36796d5f80b3c51b138bf4654e
Hendri Januri <49939976+hendrijanuri@users.noreply.github.com>
2019-10-22 22:06:35 +0700

    clean code

--- day02/test.js
+++ day02/test.js
@@ -1,12 +1,12 @@
-// console.log(a)
+// // console.log(a)
 
-var x = 'halo';
-var y = 'DUNIA';
-var z = 12345;
+// var x = 'halo';
+// var y = 'DUNIA';
+// var z = 12345;
 
-console.log(x.toUpperCase());
-console.log(y.toLowerCase());
-console.log(x.replace('ha', 'mi'))
-// console.log(x.replace(/ha/g, 'mi'))
-console.log(z.toString())
-console.log(typeof (z.toString()))
+// console.log(x.toUpperCase());
+// console.log(y.toLowerCase());
+// console.log(x.replace('ha', 'mi'))
+// // console.log(x.replace(/ha/g, 'mi'))
+// console.log(z.toString())
+// console.log(typeof (z.toString()))
