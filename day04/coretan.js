aee71666c83148ee4f5df7b7cd9f5a66c7e52a11
hendrijanuri <hendrijanuri@gmail.com>
2019-10-24 10:55:47 +0700

    add coretan and test to day 04 dir

---
+++ day04/coretan.js
@@ -0,0 +1,19 @@
+// while loop
+var x = 1
+while(x<=5){
+    console.log('nomor ' + x)
+    x++
+}
+
+// do while loop
+var y = 1
+do{
+    console.log('nomor ' + y)
+    y++
+}while(y<=7)
+
+// for loop
+var z = 1
+for (z; z <= 10; z++) {
+    console.log('nomor ' + z)
+}
