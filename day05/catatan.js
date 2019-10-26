d808c43a56f153e33a48f0de5644d73526735609
hendrijanuri <hendrijanuri@gmail.com>
2019-10-26 18:02:12 +0700

    add catatan.js

---
+++ day05/catatan.js
@@ -0,0 +1,27 @@
+// gaji hitungparkir operator functions
+// gaji karyawan gaji(jammasuk,jamkeluar,posisi)
+
+
+
+
+// for(i=0;i<5;i++){
+//     if(i==3){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+
+
+// var numb=5
+// for(a=0;a<4;a++){
+//     numb=1000
+//     for(b=0;b<4;b++){
+//         numb=5
+//         for(c=0;c<4;c++){
+//             numb=10
+//         }
+//     }
+// }
+// console.log(numb)
