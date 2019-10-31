88c51d9aee801ba5da9dfd222876065af59c9c75
hendrijanuri <hendrijanuri@gmail.com>
2019-10-31 16:50:45 +0700

    clean

--- day07/tugas03.js
+++ day07/tugas03.js
@@ -1,14 +1,14 @@
 var array = ['calya', 'xenia', 'terios', 'avanza']
-// function cari(mobil) {
-//     mobil=mobil.toLowerCase()
-//     if (array.indexOf(mobil)<0) {
-//         console.log('hasil pencarian anda nihil')
-//         return false
-//     } else {
-//         console.log('hasil pencarian anda berhasil, mobil ditemukan')
-//         return true
-//     }
-// }
+function cari(mobil) {
+    mobil=mobil.toLowerCase()
+    if (array.indexOf(mobil)<0) {
+        console.log('hasil pencarian anda nihil')
+        return false
+    } else {
+        console.log('hasil pencarian anda berhasil, mobil ditemukan')
+        return true
+    }
+}
 
 // var string='TErios'
 console.log(cari('terios'))
@@ -16,11 +16,11 @@ console.log(cari('TeRiOs'))
 console.log(cari('jimny'))
 
 // solusi mas dino
-function cari(array) {
-    for (let i=0;i<array.length;i++) {
-        if (array[i].toLowerCase()==array.toLowerCase()) {
-            return true
-        }
-    }
-    return false
-}
+// function cari(array) {
+//     for (let i=0;i<array.length;i++) {
+//         if (array[i].toLowerCase()==array.toLowerCase()) {
+//             return true
+//         }
+//     }
+//     return false
+// }
