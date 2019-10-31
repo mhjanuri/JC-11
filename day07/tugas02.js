dceb5dffc2d138c8d339bfb0d9eef4c8c1c534d5
hendrijanuri <hendrijanuri@gmail.com>
2019-10-31 10:17:17 +0700

    add comment

--- day07/tugas02.js
+++ day07/tugas02.js
@@ -1,13 +1,14 @@
 function jumlah(array) {
     var output = 0
     for (i=0;i<array.length;i++) {
-        output+=array[i]
+        output+=parseInt(array[i]) // hilangkan fungsi map di line 12 kalau pake parseInt
     }
     return output
 }
 
 var string='12345'
 
-var arr=string.split('').map(Number)
+var arr=string.split('')
+// .map(Number)
 console.log(arr)
 console.log('sum dari string set: '+string+' adalah '+jumlah(arr))
