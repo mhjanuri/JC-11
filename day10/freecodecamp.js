5cc0a9e83bb8eebd6d033e084016e54694441b53
hendrijanuri <hendrijanuri@gmail.com>
2019-11-04 17:15:37 +0700

    test

---
+++ day10/freecodecamp.js
@@ -0,0 +1,11 @@
+// var myArray = [["John", 23], ["cat", 2]];
+// myArray.push(["dog", 3])
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop('jack');
+
+console.log(myArray)
+console.log(removedFromMyArray)
