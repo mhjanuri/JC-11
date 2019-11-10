ccd4e3197263f0470d75f3d13a49bc8c59e268c2
hendrijanuri <hendrijanuri@gmail.com>
2019-11-10 14:02:59 +0700

    solusi minmax

--- day13/minmax.js
+++ day13/minmax.js
@@ -1,3 +1,16 @@
+var array = [40, 100, 1, 5, 25, 10]
+console.log('nilai Min dari ' + array + ' adalah ' + min(array))
+console.log('nilai Max dari ' + array + ' adalah ' + max(array))
+
+
+function min(a) {
+    a.sort(function (a, b) { return a - b })
+    return a[0]
+}
+function max(a) {
+    a.sort(function (a, b) { return a - b })
+    return a[(a.length) - 1]
+}
 // solusi mas dino
 const minMax=(arr=[],cond)=>{
     arr.sort((a,b)=>a-b)
