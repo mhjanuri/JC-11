943b01e829bffed5f1577f41f3e31e01bc302223
hendrijanuri <hendrijanuri@gmail.com>
2019-11-11 13:40:55 +0700

    code

--- day15/mapAla.js
+++ day15/mapAla.js
@@ -1,12 +1,12 @@
-var mapalakita=(arr,fn) => {
-    var output = []
-    for (i=0;i<arr.length;i++) {
-        output.push(fn(arr[i]))
-    }
-    return output
-}
-var arr1=[1,2,3,4,5]
-var arr2=arr1.map((val)=>val*2)
-console.log (arr2)
-var arr3=mapalakita(arr1,(val)=>val*2)
-console.log (arr3)
+// var mapalakita=(arr,fn) => {
+//     var output = []
+//     for (i=0;i<arr.length;i++) {
+//         output.push(fn(arr[i]))
+//     }
+//     return output
+// }
+
+// var arr2=arr1.map((val)=>val*2)
+// console.log (arr2)
+// var arr3=mapalakita(arr1,(val)=>val*2)
+// console.log (arr3)
