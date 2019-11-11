099f380bc95e8011bf5e8cead8f6e59192e63bf1
hendrijanuri <hendrijanuri@gmail.com>
2019-11-11 19:25:57 +0700

    done

--- day15/tugas7.js
+++ day15/tugas7.js
@@ -1,10 +1,11 @@
-const berapatahun = (awal,persen,pendatang,target) => {
-    var penduduk=awal
-    var tahun=0
-    do {
-        penduduk+=pendatang+(penduduk*(persen/100))
-        tahun ++
-    } while (penduduk<target);
-    return tahun+' tahun'
-}
-console.log(berapatahun(1000,5,50,1200))
+// const berapatahun = (awal,persen,pendatang,target) => {
+//     var penduduk=awal
+//     var tahun=0
+//     do {
+//         penduduk+=pendatang+(penduduk*(persen/100))
+//         tahun ++
+//     } while (penduduk<target);
+//     return tahun+' tahun'
+// }
+// console.log(berapatahun(1000,5,50,1200))
+
