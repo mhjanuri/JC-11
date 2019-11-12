ee2517773bbedfa84c7dc0c03c69a109f605a4ab
hendrijanuri <hendrijanuri@gmail.com>
2019-11-12 15:50:11 +0700

    done median

---
+++ day16/median.js
@@ -0,0 +1,18 @@
+const getmiddle=(array)=>{
+    // array.sort((a,b)=>a-b)
+    let median
+    if (array.length % 2 !== 0) {
+        median=array[Math.floor(array.length/2)]
+    } else {
+        const mid1 = array[(array.length / 2) - 1]
+        const mid2 = array[(array.length / 2)]
+        // median=(mid1+mid2)/2
+        median=mid1+mid2
+    }
+    return median
+}
+
+console.log(getmiddle('test'))
+console.log(getmiddle('testing'))
+console.log(getmiddle('middle'))
+console.log(getmiddle('A'))
