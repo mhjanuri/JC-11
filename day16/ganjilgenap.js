ce789896b2e337b7357d21af2d80c15329d239eb
hendrijanuri <hendrijanuri@gmail.com>
2019-11-12 15:14:24 +0700

    done ganjilgenap

---
+++ day16/ganjilgenap.js
@@ -0,0 +1,11 @@
+const ganjilgenap=(plat,tanggal)=> {
+    var arr=plat.split(' ')
+    if (arr[1] % 2 == 0 && tanggal % 2 == 0) {
+        return 'anda boleh lewat'
+    } else if (arr[1] % 2 !== 0 && tanggal % 2 !== 0) {
+        return 'anda boleh lewat'
+    } else {
+        return 'anda tidak boleh lewat'
+    }  
+}
+console.log(ganjilgenap('B 1234 AR',24))
