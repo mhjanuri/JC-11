b16a52a3e9f7bab120193b5300747b7b942a4424
hendrijanuri <hendrijanuri@gmail.com>
2019-11-17 22:21:04 +0700

    test

--- test/kpk-fpb.js
+++ test/kpk-fpb.js
@@ -1,31 +1,33 @@
-function gcd(a, b) {
-    if (a == 0) {
-        return b
-    }
-    if (b == 0) {
-        return a
-    }
-    if (a == b) {
-        return a
-    }
-    if (a > b) {
-        return gcd(a - b, b);
-    }
-    return gcd(a, b - a);
-}
-
-// function gcd(x,y) {
-//     while (y) {
-//         var t = y;
-//         y = x % y;
-//         x = t;
+// function gcd(a, b) {
+//     if (a == 0) {
+//         return b
 //     }
-//     return x;
+//     if (b == 0) {
+//         return a
+//     }
+//     if (a == b) {
+//         return a
+//     }
+//     if (a > b) {
+//         return gcd(a - b, b);
+//     }
+//     return gcd(a, b - a);
 // }
+
+function gcd(x,y) {
+    let a=x
+    let b=y
+    while (y) {
+        var t = y;
+        y = x % y;
+        x = t;
+    }
+    // return x;
+    return console.log("FPB "+a+" & "+b+ " = " + x)
+}
     
-var a = 2
-var b = 3
-console.log("FPB of " + a + " and " + b + " is " + gcd(a, b))
-
-
+// var a = 2
+// var b = 3
+// console.log("FPB of " + a + " and " + b + " is " + gcd(a, b))
+console.log(gcd(2,3))
 
