354d45006ebb177cb7dcd8a8f065fa57f42309b0
hendrijanuri <hendrijanuri@gmail.com>
2019-11-17 22:50:42 +0700

    fix undefined

--- test/kpk-fpb.js
+++ test/kpk-fpb.js
@@ -9,8 +9,8 @@ function fpb_kpk(x, y) {
     }
     let kpk = (a * b) / x
     // return x;
-    return console.log("FPB " + a + " & " + b + " = " + x + "\nKPK " + a + " & " + b + " = " + kpk)
+    return ("FPB " + a + " & " + b + " = " + x + "\nKPK " + a + " & " + b + " = " + kpk)
 }
 
 console.log(fpb_kpk(2, 3))
-// console.log(fpb_kpk(5, 7))
+console.log(fpb_kpk(5, 7))
