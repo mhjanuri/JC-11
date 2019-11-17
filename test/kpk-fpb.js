1945659e58cb593d85dcbb1bbf9830e130359ad2
hendrijanuri <hendrijanuri@gmail.com>
2019-11-17 22:42:36 +0700

    done

--- test/kpk-fpb.js
+++ test/kpk-fpb.js
@@ -1,19 +1,3 @@
-// function gcd(a, b) {
-//     if (a == 0) {
-//         return b
-//     }
-//     if (b == 0) {
-//         return a
-//     }
-//     if (a == b) {
-//         return a
-//     }
-//     if (a > b) {
-//         return gcd(a - b, b);
-//     }
-//     return gcd(a, b - a);
-// }
-
 function fpb_kpk(x, y) {
     let a=x
     let b=y
