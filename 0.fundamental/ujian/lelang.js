eb838ec554fc59898520c57e3da4d3b3c36c2af8
hendrijanuri <hendrijanuri@gmail.com>
2019-11-20 09:06:07 +0700

    fix

--- 0.fundamental/ujian/lelang.js
+++ 0.fundamental/ujian/lelang.js
@@ -1,25 +1,45 @@
-const lelang = (a) => {
-    var harga = 10000
-    var menit = 0
+// const lelang = (a) => {
+//     var harga = 10000
+//     var menit = 0
     
-    while (menit < a) {
-        if (a % 4 == 0) {
-            var discount10 = Math.ceil(harga * (10 / 100))
-            harga = harga + discount10
-            menit++
-        } else {
-            var discount20 = Math.ceil(harga * (20/100))
-            harga = harga + discount20
-            menit++
+//     while (menit < a) {
+//         if (a % 4 == 0) {
+//             var discount10 = Math.ceil(harga * (10 / 100))
+//             harga = harga + discount10
+//             menit++
+//         } else {
+//             var discount20 = Math.ceil(harga * (20/100))
+//             harga = harga + discount20
+//             menit++
+//         }
+//     }
+
+//     if (harga <= 30000000) {
+//         return 'menit ke ' + a + ' hasilnya ' + harga
+//     } else {
+//         return 'menit ke ' + a + ' barang sudah terjual'
+//     }
+// }
+
+const LelangZayn = (waktu) => {
+
+    HargaAwal = 10000
+    for (i = 1; i <= waktu; i++) {
+        if (i % 4 == 0) {
+            HargaAwal = HargaAwal + Math.ceil(HargaAwal * (10 / 100))
+        }
+        else {
+            HargaAwal = HargaAwal + Math.ceil(HargaAwal * (20 / 100))
+        }
+        if (HargaAwal >= 30000000) {
+            return 'menit ke ' + waktu + ' barang sudah terjual'
         }
     }
-
-    if (harga <= 30000000) {
-        return 'menit ke ' + a + ' hasilnya ' + harga
-    } else {
-        return 'menit ke ' + a + ' barang sudah terjual'
-    }
+    return 'menit ke ' + waktu + ' hasilnya ' + HargaAwal
 }
-console.log(lelang(2))
-console.log(lelang(50))
-console.log(lelang(49))
+
+
+console.log(LelangZayn(50))
+// console.log(lelang(2))
+// console.log(lelang(50))
+// console.log(lelang(49))
