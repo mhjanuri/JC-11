3f5dbad1978bccd5035f49eb6045c9458bb7a5a7
hendrijanuri <hendrijanuri@gmail.com>
2019-11-27 23:51:25 +0700

    done 404

--- 1.front-end/day23/reactjw/src/pages/error404.jsx
+++ 1.front-end/day23/reactjw/src/pages/error404.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import Jumbotron1 from './../components/jumbotron1'
+import { Link } from 'react-router-dom'
 
 class Error404 extends Component {
     constructor(props) {
@@ -8,8 +8,23 @@ class Error404 extends Component {
     }
     render() { 
         return (
-            <div style={{ color: 'white' }}>
-                Error Pak!!!
+            <div>
+                <video autoPlay muted loop id="myVideo404" onclick="myFunction()">
+                    <source src="https://cdn.jwplayer.com/videos/EUijQ1Ay-St99WDS4.mp4" type="video/mp4" />
+                </video>
+                <div className="content-404">
+                    <div className="center-box-404">
+                        <div>
+                            <h1>404: Page Not Found</h1> <br/>
+                            <p>We couldn't find that page, but here are some jellyfish.</p> <br />
+                            <button className="myBtn404" style={{textDecoration:"none",color:"white"}}>
+                                <Link to={'/'} className="link-myBtn404">
+                                    Go Home
+                                </Link>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
