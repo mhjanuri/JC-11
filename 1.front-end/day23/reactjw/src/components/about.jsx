d141aaffa132d8069db5ebe90343fcce5ece59e8
hendrijanuri <hendrijanuri@gmail.com>
2019-11-27 11:38:43 +0700

    fix

---
+++ 1.front-end/day23/reactjw/src/components/about.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    state = {  }
+    
+    render() { 
+        return (
+            <div style={{color:'white'}}>
+                ini about
+            </div>
+        );
+    }
+}
+ 
+export default About;
