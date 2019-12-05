e533eafb41f95abae87b9a6cdb05cb2761d0655e
hendrijanuri <hendrijanuri@gmail.com>
2019-12-05 23:37:06 +0700

    done

--- 1.front-end/bioskop-projek/bioskopui/src/pages/home.jsx
+++ 1.front-end/bioskop-projek/bioskopui/src/pages/home.jsx
@@ -22,15 +22,29 @@ class Home extends Component {
     renderMovies=()=>{
         return this.state.dataMovies.map((val,index)=>{
             return (
-                
+
+                <div key={index} className="col-md-3 py-5 pr-3 pl-1">
+                    <div className="card kartu" style={{ width: '100%' }}>
+                        <div className="gambaar1">
+                            <img src={val.image} className="card-img-top kartu gambar" alt="..." />
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title">{val.title}</h5>
+                            <p className="card-text">{val.sinopsis}</p>
+                            <a href="#" classname="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
             )
         })
     }
 
     render() { 
         return (
-            <div>
-                ini
+            <div className='mx-5'>
+                <div class="row py-5 " style={{paddingLeft: '10%', paddingRight: '10%'}}>
+                    {this.renderMovies()}
+                </div>
             </div>
         )    
     }
