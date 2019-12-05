5d070282050ef558823a52d84d9c894f1b968878
hendrijanuri <hendrijanuri@gmail.com>
2019-12-05 22:24:57 +0700

    home

--- 1.front-end/bioskop-projek/bioskopui/src/index.js
+++ 1.front-end/bioskop-projek/bioskopui/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- 1.front-end/bioskop-projek/bioskopui/src/pages/home.jsx
+++ 1.front-end/bioskop-projek/bioskopui/src/pages/home.jsx
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
+import Axios from 'axios'
+
+// eslint-disable-next-line
+const url='http://localhost:2000'
 
 class Home extends Component {
-    state = {  }
+    state = {
+        dataMovies:[]
+    }
+
+    componentDidMount(){
+        Axios.get(`${url}/movies`)
+        .then((res)=>{
+            this.setState({dataMovies:res.data})
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
+    renderMovies=()=>{
+        return this.state.dataMovies.map((val,index)=>{
+            return (
+                
+            )
+        })
+    }
+
     render() { 
         return (
             <div>
