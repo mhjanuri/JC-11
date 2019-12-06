0cbf4c56d52075e6f2c4ebd6a379ed3103506b27
hendrijanuri <hendrijanuri@gmail.com>
2019-12-06 08:56:53 +0700

    fix error

--- 1.front-end/bioskop-projek/bioskopui/src/pages/home.jsx
+++ 1.front-end/bioskop-projek/bioskopui/src/pages/home.jsx
@@ -13,6 +13,7 @@ class Home extends Component {
         Axios.get(`${url}/movies`)
         .then((res)=>{
             this.setState({dataMovies:res.data})
+            console.log(res.data)
         })
         .catch((err)=>{
             console.log(err)
@@ -31,7 +32,8 @@ class Home extends Component {
                         <div className="card-body">
                             <h5 className="card-title">{val.title}</h5>
                             <p className="card-text">{val.sinopsis}</p>
-                            <a href="#" classname="btn btn-primary">Go somewhere</a>
+                            {/* eslint-disable-next-line */}
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
@@ -42,7 +44,7 @@ class Home extends Component {
     render() { 
         return (
             <div className='mx-5'>
-                <div class="row py-5 " style={{paddingLeft: '10%', paddingRight: '10%'}}>
+                <div className="row py-5 " style={{paddingLeft: '10%', paddingRight: '10%'}}>
                     {this.renderMovies()}
                 </div>
             </div>
