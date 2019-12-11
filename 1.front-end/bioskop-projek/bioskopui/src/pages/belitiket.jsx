5aadf8c7adaf798827d27d3374cdfa5e92fc172a
hendrijanuri <hendrijanuri@gmail.com>
2019-12-11 14:11:48 +0700

    m

--- 1.front-end/bioskop-projek/bioskopui/src/pages/belitiket.jsx
+++ 1.front-end/bioskop-projek/bioskopui/src/pages/belitiket.jsx
@@ -1,16 +1,122 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import Axios from 'axios'
+import { APIURL } from '../support/ApiUrl';
 
-class BeliTiket extends Component {
+class Belitiket extends Component {
     state = {
-        
+        datamovie:{},
+        seats:260,
+        baris:0,
+        booked:[],
+        loading:true,
+        jam:12,
+        pilihan:[]
     }
+
+    componentDidMount() {
+        // console.log(this.props.location.state)
+        var studioID = this.props.location.state.studioID
+        var movieID = this.props.location.state.id
+        Axios.get(`${APIURL}/studios/${studioID}`)
+        .then((res1)=>{
+            Axios.get(`${APIURL}/orders?movieId=${movieID}&jadwal=${this.state.jam}`)
+            .then((res2)=>{
+                var arrAxios=[]
+                res2.data.forEach((val)=>{
+                    arrAxios.push(Axios.get(`${APIURL}/ordersDetails?orderId=${val.id}`))
+                })
+                var arrAxios2=[]
+                Axios.all(arrAxios)
+                .then((res3)=>{
+                    res3.forEach((val)=>{
+                        arrAxios2.push(...val.data)
+                    })
+                    this.setState({
+                        datamovie: this.props.location.state,
+                        seats: res1.data.jumlahKursi,
+                        baris: res1.data.jumlahKursi/20,
+                        booked: arrAxios2,
+                        loading: false,
+                    })
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }).catch((err2) => {
+                console.log(err2)
+            })
+        }).catch((err1)=>{
+            console.log(err1)
+        })
+    }
+
+    renderseat = () => {
+        var arr=[]
+        for(let i=0; i<this.state.seats/this.state.baris; i++){
+            arr.push([])
+            for(let j=0; j<this.state.seats/this.state.baris; j++){
+                arr[i].push(i)
+            }
+        }
+        console.log(this.state.booked)
+        for(let j=0; j<this.state.booked.length; j++){
+            arr[this.state.booked[j].row][this.state.booked[j].seat]=3
+        }
+        for(let a=0; a<this.state.pilihan.length; a++){
+            arr[this.state.booked[j].row][this.state.booked[j].seat]=2
+        }
+        var alpahabet='abcdefghijklmnopqrstuvwxyz'.toUpperCase()
+        var jsx=arr.map((val,index)=>{
+            return(
+                <div key={index}>
+                 { 
+                    val.map((val,i)=>{
+                        if(val1===1){
+                            return(
+                                <button key={i} disabled className='rounded btn-disabled mr-2 mt-2 bg-danger text-center'>
+                                    {alpahabet[index]+(i+1)}
+                                </button>
+                            )
+                        }else if(val1===2){
+                            return(
+                            <button key={i}className='rounded btn-disabled mr-2 mt-2 bg-danger text-center'>
+                            {alpahabet[index]+(i+1)}>
+                            </button>
+                         )
+                    }
+                    return(
+                         <button key={i}className='rounded btn-disabled mr-2 mt-2 bg-danger text-center'>
+                            {alpahabet[index]+(i+1)}>
+                            </button>
+                         )
+                         })
+                 }
+                </div>    
+            )
+        })
+        return jsx
+    }
+
     render() { 
+        if (this.props.location.state && this.props.AuthLog) {
+            return (
+                <div>
+                    beli tiket
+                    {this.state.loading ? null : this.renderseat()}
+                </div>
+            );
+        }
         return (
             <div>
-                
+                404 not found
             </div>
-        );
+        )
     }
 }
  
-export default BeliTiket;
+const MapstateToprops = (state) => {
+    return {
+        AuthLog: state.Auth.login
+    }
+}
+export default connect(MapstateToprops)(Belitiket);
--- 1.front-end/bioskop-projek/bioskopui/src/pages/moviedetail.jsx
+++ 1.front-end/bioskop-projek/bioskopui/src/pages/moviedetail.jsx
@@ -1,71 +1,78 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import { APIURL } from '../support/ApiUrl';
-import { Modal, ModalBody } from 'reactstrap';
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
+// import Belitiket from './belitiket'
+import { Modal, ModalBody, ModalFooter } from 'reactstrap'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 class MovieDetail extends Component {
     state = {
-        datadetailfilm:{},
-        traileropen:false,
-        notloginyet:false,
-        kelogin:false,
-        belitiketok:false
-
+        datadetailfilm: {},
+        traileropen: false,
+        notloginyet: false,
+        kelogin: false,
+        belitiketok: false
     }
-
-    componentDidMount(){
+    componentDidMount() {
         Axios.get(`${APIURL}/movies/${this.props.match.params.id}`)
-        .then(res=>{
-            this.setState({datadetailfilm:res.data})
-            console.log(res.data)
-        }).catch(err=>{
-            console.log(err)
-        })
+            .then(res => {
+                this.setState({ datadetailfilm: res.data })
+            }).catch(err => {
+                console.log(err)
+            })
     }
 
-    onBeliTicketClick=()=>{
-        if(this.props.AuthLog){
-            this.setState({belitiketok:true})
-        }else{
-            this.setState({notloginyet:true})
+    onBeliTiketClick = () => {
+        if (this.props.AuthLog) {
+            this.setState({ belitiketok: true })
+        } else {
+            this.setState({ notloginyet: true })
         }
     }
 
 
     render() {
-        if(this.state.kelogin){
+        if (this.state.kelogin) {
             return <Redirect to={'/login'} />
-        } 
+        }
+        if (this.state.belitiketok) {
+            return <Redirect to={{ pathname: '/belitiket', state: this.state.datadetailfilm }} />
+        }
         return (
             <div>
-                <Modal isopen={this.state.traileropen} size='lg' toogle={()=>this.setState({traileropen:false})}
-                 className='trailer' wrapClassName='trailer'contentClassName='trailer' > 
-                    <ModalBody className='p-0 bg-transparent' >
-                        <iframe width="100%" height="100%" title={this.state.datadetailfilm.title} src={this.state.datadetailfilm.trailer} 
-                            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> 
+                <Modal isOpen={this.state.traileropen} size='lg' toggle={() => this.setState({ traileropen: false })}
+                    contentClassName=' trailer' >
+                    <ModalBody className='p-0 bg-transparent'>
+                        <iframe width="100%" title={this.state.datadetailfilm.title} height="100%" src={this.state.datadetailfilm.trailer}
+                            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                         </iframe>
                     </ModalBody>
                 </Modal>
-                <div className="row p-3">
-                    <div className="col md-5">
-                        <img src={this.state.datadetailfilm.image} height='400' alt='film' />
-                        <div className='mt-3' style={{fontSize:'30px'}}>
+                <Modal isOpen={this.state.notloginyet} centered toggle={() => this.setState({ notloginyet: false })}>
+                    <ModalBody>
+                        anda belum login, untuk melakukan order silakan login dahulu
+                    </ModalBody>
+                    <ModalFooter>
+                        <button onClick={() => this.setState({ kelogin: true })}>Ok</button>
+                    </ModalFooter>
+                </Modal>
+                <div className="row p-3 mx-3 my-4">
+                    <div className="col-md-4">
+                        <img src={this.state.datadetailfilm.image} height='400' alt="film" />
+                        <div className='mt-3' style={{ fontSize: '30px' }}>
                             {this.state.datadetailfilm.title}
                         </div>
                     </div>
-                    <div className='col-md-2'>
-                        <div className='mt-1' >
+                    <div className="col-md-2">
+                        <div className='mt-1'>
                             Title <span className='ml-4'>:</span>
                         </div>
-                    </div>
-                    <div className='col-md-2'>
-                        <div className='mt-1' >
-                            Sinopsis <span className='ml-4'>:</span>
+                        <div className='mt-1'>
+                            Sinopsis <span className='ml-2'>:</span>
                         </div>
                     </div>
-                    <div className='col-md-6'>
+                    <div className="col-md-6">
                         <div className='mt-1'>
                             {this.state.datadetailfilm.title}
                         </div>
@@ -73,8 +80,8 @@ class MovieDetail extends Component {
                             {this.state.datadetailfilm.sinopsis}
                         </div>
                         <div className='mt-3'>
-                            <button className='mr-3 btn btn-primary' >Beli Tiket</button>
-                            <button className='mr-3 btn btn-outline-warning' onClick={()=>this.setState({traileropen:true})} >Trailer</button>
+                            <button className='mr-3 btn btn-primary' onClick={this.onBeliTiketClick} >Beli tiket</button>
+                            <button className=' btn btn-outline-warning' onClick={() => this.setState({ traileropen: true })}>Trailer</button>
                         </div>
                     </div>
                 </div>
@@ -82,11 +89,9 @@ class MovieDetail extends Component {
         );
     }
 }
-
-const MapStateToProps=(state)=>{
+const MapstateToprops = (state) => {
     return {
-        AuthLog:state.Auth.login
+        AuthLog: state.Auth.login
     }
 }
- 
-export default connect(MapStateToProps) (MovieDetail);
+export default connect(MapstateToprops)(MovieDetail);
