5b2d84220779716d287ea1311601c6166b0a329b
hendrijanuri <hendrijanuri@gmail.com>
2020-01-09 11:33:35 +0700

    c

---
+++ 2.back-end/day01/index.js
@@ -0,0 +1,24 @@
+// const nama = require('./day1')
+// console.log(nama.hurufH(11))
+
+var objekku={
+    bobi:()=>{
+        return [0,1,{
+            fakhran:[0,1,{
+                dzaky:()=>{
+                    return {
+                        ragiel:[0,()=>{
+                            return {
+                                kartika:(a)=>{
+                                    return [0,1,2,'berhasil'+a]
+                                }
+                            }
+                        }]
+                    }
+                }
+            }]
+        }]
+    }
+}
+
+console.log(objekku.bobi()[2].fakhran[2].dzaky().ragiel[1]().kartika(' :p')[3])c
--- 2.back-end/day02/day02.js
+++ 2.back-end/day02/day02.js
@@ -1,16 +1,34 @@
-var timers = require('timers')
-var assert = require('assert')
+// var timers = require('timers')
+// var assert = require('assert')
 
 
 
 
-var minum = {
-    kopi:['luwak','hitam','susu']
-}
+// var minum = {
+//     kopi:['luwak','hitam','susu']
+// }
 
-try {
-    assert.equal(minum.kopi.length,3)
-} catch (error) {
-    console.log('masuk catch')
-    throw error
-}
+// try {
+//     assert.equal(minum.kopi.length,3)
+// } catch (error) {
+//     console.log('masuk catch')
+//     throw error
+// }
+
+
+const os = require('os');
+var namaCPU = os.hostname(); 
+var osTipe = os.type();
+var osPlatform = os.platform();
+var osRilis = os.release();
+var dirAwal = os.homedir();
+var ramSisa = os.freemem();
+var ramTotal = os.totalmem();
+
+console.log(namaCPU)
+console.log(osTipe)
+console.log(osPlatform)
+console.log(osRilis)
+console.log(dirAwal)
+console.log(ramSisa)
+console.log(ramTotal)
