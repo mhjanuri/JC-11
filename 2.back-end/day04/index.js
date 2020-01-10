f60c5d353a8f10946e7eb10dcafead1337e52f39
hendrijanuri <hendrijanuri@gmail.com>
2020-01-10 10:15:07 +0700

    abc 5 dasar

---
+++ 2.back-end/day04/index.js
@@ -0,0 +1,6 @@
+const chalk=require('chalk')
+
+console.log(chalk.magenta('ini talkshow'))
+console.log(chalk.blue("ini talkshow"));
+console.log(chalk.green("ini talkshow"));
+console.log(chalk.grey("ini talkshow"));
