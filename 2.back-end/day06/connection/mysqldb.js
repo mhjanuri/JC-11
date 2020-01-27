ff0b8e49179f3e0076e956812acb4d370a276578
hendrijanuri <hendrijanuri@gmail.com>
2020-01-27 14:55:30 +0700

    db

--- 2.back-end/day06/connection/mysqldb.js
+++ 2.back-end/day06/connection/mysqldb.js
@@ -1,11 +1,17 @@
 const mysql=require('mysql')
 
 const db=mysql.createConnection({
-    host:'db4free.net',
-    user:'hendrijanuri',
-    password:'4aG5DA!WWKwwDDK',
-    database:'testdbbioskop',
-    port:'3306'
+    // host:'db4free.net',
+    // user:'hendrijanuri',
+    // password:'4aG5DA!WWKwwDDK',
+    // database:'testdbbioskop',
+    // port:'3306'
+
+    host: 'localhost',
+    user: 'root',
+    password: '!Anakharam123',
+    database: 'day07',
+    port: '3306'
 })
 
 
