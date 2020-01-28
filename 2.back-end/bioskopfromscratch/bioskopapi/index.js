dd2320f9e1e32ee3730c8aecb2350633d919ba1e
hendrijanuri <hendrijanuri@gmail.com>
2020-01-28 13:42:41 +0700

    ghj

--- 2.back-end/bioskopfromscratch/bioskopapi/index.js
+++ 2.back-end/bioskopfromscratch/bioskopapi/index.js
@@ -19,11 +19,11 @@ app.get('/', (req, res) => {
 const {Authrouter} = require('./router')
 app.use('/user',Authrouter)
 
-const hasspassword = require('./helper/encrypt')
-// app.get('/encrypt', (req, res) => {
-//     const {password} = req.query
-//     var hasilencrypt=hasspassword(password)
-//     return res.status(200).send({encrypt:hasilencrypt, passlama:password})
-// })
+const hashpassword = require('./helper/encrypt')
+app.get('/encrypt', (req, res) => {
+    const {pass} = req.query
+    var hasilencrypt=hashpassword(pass)
+    return res.status(200).send({encrypt:hasilencrypt, passlama:pass})
+})
 
 app.listen(PORT, () => console.log(`aktif di port ${PORT}`))
