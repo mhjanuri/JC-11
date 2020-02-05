678d07e0b966025d78650afcbee81f12208362b5
hendrijanuri <hendrijanuri@gmail.com>
2020-02-05 16:00:22 +0700

    redux

--- 2.back-end/uiapiuploadfoto/ui/src/App.js
+++ 2.back-end/uiapiuploadfoto/ui/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import Transaksi from './pages/transaksi'
 
 function App() {
   return (
--- 2.back-end/uiapiuploadfoto/ui/src/index.js
+++ 2.back-end/uiapiuploadfoto/ui/src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import './supports/fontawesome-free/css/fontawesome.min.css'
+// import './supports/fontawesome-free/css/all.css'
+// import './supports/css/style.css'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
+// import { Provider } from 'react-redux'
+// import reducers from './redux/reducers'
+// import ReduxThunk from 'redux-thunk'
+// import { createStore, applyMiddleware } from 'redux';
+// const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
