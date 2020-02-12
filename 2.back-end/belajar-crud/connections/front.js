ba049b53d77fa56533305b79cecfa9d184c357ee
hendrijanuri <hendrijanuri@gmail.com>
2020-02-12 09:02:18 +0700

    mobile

--- 2.back-end/belajar-crud/connections/front.js
+++ 2.back-end/belajar-crud/connections/front.js
@@ -1,5 +1,7 @@
 module.exports = 
 `
     <h1>Hello from the server siiiiiiiiiiiide!</h1> 
-    <img src="https://pbs.twimg.com/profile_images/657199367556866048/EBEIl2ol.jpg" />
+    <a href="https://github.com/hendrijanuri">
+        <img src="https://pbs.twimg.com/profile_images/657199367556866048/EBEIl2ol.jpg"  />
+    </a>
 `
